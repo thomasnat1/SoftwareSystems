0d82501e15f3d4366cea3d567a875475b987571e
tnattestad <tnattestad@ubuntu.(none)>
2014-02-05 13:27:30 -0500

    split into a few function and changed error in book where it did not increase the count when 2 came up

--- hw01/cards.c
+++ hw01/cards.c
@@ -1,36 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+void updateCount(int val, int *count){
+	if((val >= 2) && (val < 7)){
+		*count = *count + 1;
+	}else if(val == 10){
+		*count = *count - 1;
+	}
+}
+
+int readCard(){
 	char card_name[3];
+	puts("Enter the card_name: ");
+	scanf("%2s", card_name);
+	switch(card_name[0]){
+	case 'K':
+	case 'Q':
+	case 'J': 
+		return 10;
+	case 'A':
+		return 11;
+	case 'X':
+		return 0;
+	default:
+		return atoi(card_name);
+	}
+}
+
+int main(){
 	int count = 0;
-	while(card_name[0] != 'X'){
-		puts("Enter the card_name: ");
-		scanf("%2s", card_name);
-		int val = 0;
-		switch(card_name[0]){
-		case 'K':
-		case 'Q':
-		case 'J': 
-			val = 10;
-			break;
-		case 'A':
-			val = 11;
-			break;
-		case 'X':
+	int val = -1;
+	while(val != 0){
+		val = readCard();
+		if(val == 0){
 			continue;
-		default:
-			val = atoi(card_name);
-			if((val < 1) || (val > 10)){
-				puts("I don't understand that value!");
-				continue;
-			}
+		}else if((val < 1) || (val > 11)){
+			puts("I don't understand that value!");
+			continue;
 		}
-		if((val > 2) && (val < 7)){
-			count++;
-		}else if(val == 10){
-			count--;
-		}
+
+		updateCount(val, &count);
 		printf("current count: %i\n", count);
 	}
 	return 0;
